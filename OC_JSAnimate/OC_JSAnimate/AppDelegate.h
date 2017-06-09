11c30bc3c7b49e683f590d5a217a675d270b4042
lkk2016 <631813042@qq.com>
2017-06-09 16:34:27 +0800

    初始化

---
+++ OC_JSAnimate/OC_JSAnimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_JSAnimate
+//
+//  Created by 宇航 on 17/6/9.
+//  Copyright © 2017年 HWD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC_JSAnimate/OC_JSAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_JSAnimate
+//
+//  Created by 宇航 on 17/6/9.
+//  Copyright © 2017年 HWD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
